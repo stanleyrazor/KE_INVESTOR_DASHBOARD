76b0f5ad6796bd6b821679bd00790a04c9d7c306
Stanley sayianka <stanleysayankasss@gmail.com>
2022-04-07 12:13:30 +0300

    Add files via upload
    Updatd the file_dump folder to contain the new data files

--- data/file_dump/scraper file.R
+++ data/file_dump/scraper file.R
@@ -8,11 +8,11 @@ setwd('C:/Users/stanley/Desktop/MISCELLANEOUS R/nse-scrape/WSJ/file_dump')
 
 # loading NSE data
 nse_sec <- read_xlsx('C:/Users/stanley/Desktop/MISCELLANEOUS R/nse-scrape/Complete-List-of-Listed-Companies-on-Nairobi-Securities-Exchange-Jan-2020-Best.xlsx')
-nse_sec <- nse_sec[-(66:71), -4]
-nse_sec$Industry <- ifelse(test = nse_sec$Industry == 'Investment Services',
-                           yes = 'Investment',
-                           no = nse_sec$Industry)
-colnames(nse_sec) <- c('Company Name', 'Ticker', 'Industry')
+#nse_sec <- nse_sec[-(66:71), -4]
+#nse_sec$Industry <- ifelse(test = nse_sec$Industry == 'Investment Services',
+#                           yes = 'Investment',
+#                           no = nse_sec$Industry)
+#colnames(nse_sec) <- c('Company Name', 'Ticker', 'Industry')
 
 ticker_list <- nse_sec$Ticker
 for (i in 1:length(ticker_list))
